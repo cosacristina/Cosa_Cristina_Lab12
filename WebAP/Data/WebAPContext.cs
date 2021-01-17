035ceabe0e9b728601cc3673ead3e01ef3a8586f
Cristina Cosa <cosa.cristinamaria@yahoo.com>
2021-01-17 21:50:23 +0200

    Add project files.

---
+++ WebAP/Data/WebAPContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAP.Models;
+
+namespace WebAP.Data
+{
+    public class WebAPContext : DbContext
+    {
+        public WebAPContext (DbContextOptions<WebAPContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAP.Models.ShopList> ShopList { get; set; }
+    }
+}
